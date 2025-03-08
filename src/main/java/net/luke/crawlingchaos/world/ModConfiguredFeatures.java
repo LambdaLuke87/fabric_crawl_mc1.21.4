07e0bab058de4d35a91c60e0e875fbd2a9364be0
LambdaLuke87 <LambdaLuke87@users.noreply.github.com>
2025-03-08 00:12:41 -0300

    Bug Tree Update part 4: Improved Tree Generation WIP

--- src/main/java/net/luke/crawlingchaos/world/ModConfiguredFeatures.java
+++ src/main/java/net/luke/crawlingchaos/world/ModConfiguredFeatures.java
@@ -2,19 +2,28 @@ package net.luke.crawlingchaos.world;
 
 import net.luke.crawlingchaos.CrawlingChaos;
 import net.luke.crawlingchaos.block.ModBlocks;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.PropaguleBlock;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.intprovider.ConstantIntProvider;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.FeatureConfig;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
-import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
+import net.minecraft.world.gen.foliage.LargeOakFoliagePlacer;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
-import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
+import net.minecraft.world.gen.stateprovider.RandomizedIntBlockStateProvider;
+import net.minecraft.world.gen.treedecorator.AttachedToLeavesTreeDecorator;
+import net.minecraft.world.gen.treedecorator.LeavesVineTreeDecorator;
+import net.minecraft.world.gen.trunk.LargeOakTrunkPlacer;
+
+import java.util.List;
 
 public class ModConfiguredFeatures {
     public static final RegistryKey<ConfiguredFeature<?, ?>> BUG_OAK_KEY = registerKey("bug_oak");
@@ -22,12 +31,16 @@ public class ModConfiguredFeatures {
     public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> context) {
         register(context, BUG_OAK_KEY, Feature.TREE, new TreeFeatureConfig.Builder(
                 BlockStateProvider.of(ModBlocks.BUG_OAK_LOG),
-                new StraightTrunkPlacer(5, 6, 3),
+                new LargeOakTrunkPlacer(7, 8, 3),
 
                 BlockStateProvider.of(ModBlocks.BUG_OAK_LEAVES),
-                new BlobFoliagePlacer(ConstantIntProvider.create(4), ConstantIntProvider.create(1), 3),
+                new LargeOakFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(4), 4),
 
-                new TwoLayersFeatureSize(1, 0, 2)).build());
+                new TwoLayersFeatureSize(1, 0, 2))
+                .decorators(List.of(new LeavesVineTreeDecorator(0.125F),
+                        new AttachedToLeavesTreeDecorator(0.14F, 1, 0,
+                                new RandomizedIntBlockStateProvider(BlockStateProvider.of(Blocks.COBWEB),
+                                        PropaguleBlock.AGE, UniformIntProvider.create(0, 4)), 2, List.of(Direction.DOWN)))).build());
     }
 
     public static RegistryKey<ConfiguredFeature<?, ?>> registerKey(String name) {
